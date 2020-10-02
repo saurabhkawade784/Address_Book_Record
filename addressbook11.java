77c41d980fd72d93eecd61865f43d119306b5934
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-10-02 13:33:11 +0530

    Address book file

---
+++ addressbook11.java
@@ -0,0 +1,13 @@
+package Addressbook;
+
+public class addressbook {
+	    public static String database="bhoodev";
+	    public static String user="root";
+	    public static String password="Bhoodev@14";
+	    public static void main(String[] args)
+	    {
+	                new MainPage().setVisible(true);    
+	    }
+	        
+	}
+
